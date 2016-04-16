626aea73ede2e446cdf7ed29b686d6bdb1d6c90a
Ben Godfrey <benjetson5236@gmail.com>
2016-04-16 18:16:56 -0400

    Got the burger menu working.

--- js/angularBootstrap.js
+++ js/angularBootstrap.js
@@ -1,11 +1,69 @@
 angular
 .module('mannRoboWeb', ['ngMaterial'])
-.controller('MainCtrl', function($scope, $timeout, $mdSidenav, $log) {
+.controller('MainCtrl', function($scope, $timeout, $mdSidenav, $mdDialog, $log) {
     //$scope.hello = 'yello';
     $scope.toggleNav = buildToggler('left');
     $scope.isNavOpen = function() {
         return $mdSidenav('left').isOpen();
     }
+    
+    $scope.menu = [
+        {
+            text: 'Home',
+            href: "index.html",
+            icon: "home"
+        },
+        {
+            text: 'About Us',
+            href: 'about.html',
+            icon: 'people'
+        },
+        {
+            text: 'Gallery',
+            href: 'gallery.html',
+            icon: 'photo'
+        },
+        {
+            text: 'News',
+            href: 'news.html',
+            icon: 'chrome_reader_mode'
+        },
+        {
+            text: 'Calendar',
+            href: 'calendar.html',
+            icon: 'today'
+        },
+        {
+            text: 'Resources',
+            href: 'resources.html',
+            icon: 'book'
+        }
+    ];
+    
+    $scope.lowerMenu = [
+        {
+            text: 'Contact Us',
+            href: 'contact.html',
+            onclick: 'return true;',
+            icon: 'email'
+        },
+        {
+            text: 'Settings',
+            onclick: 'settingsHandler();',
+            icon: 'settings'
+        }
+    ];
+    
+    $scope.settingsHandler = function($scope) {
+        $mdDialog.show(
+            $mdDialog.alert({
+                title: 'Settings',
+                textContent: 'Settings has been disabled while site configuration is in progress. ' + 
+                             'Check back later.',
+                ok: 'close'
+            })
+        );
+    };    
 
     function debounce(func, wait, context) {
       var timer;
@@ -39,7 +97,7 @@ angular
         $mdSidenav(navID)
           .toggle()
           .then(function () {
-            $log.debug("toggle " + navID + " is done");
+            //$log.debug("toggle " + navID + " is done");
           });
       }
     }
